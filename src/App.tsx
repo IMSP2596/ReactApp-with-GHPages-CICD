c55951c9781e10d8eff5247024ff60c509ccfd69
Shubham Patil <65598491+IMSP2596@users.noreply.github.com>
2024-12-19 18:16:42 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ const App=()=>{
         <>
         <h1>{`Hello World `}</h1>
         
-        <h1>{`mode  ${process.env.NODE_ENV}`}</h1>
+        <h2>{`mode  ${process.env.NODE_ENV}`}</h2>
+
+        <h3>{`repo name ${process.env.repo_name}`}</h3>
         </>
     )
 }
