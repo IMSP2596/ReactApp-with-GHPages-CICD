99e5b5ea1421e1de559c439b77aa37cff87f34bc
Shubham Patil <Shubham.Patil@ingrammicro.com>
2024-12-20 17:26:10 +0530

    Adding Template oriented changes

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,30 +1,35 @@
-const path =require('path');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports={
-    entry:path.resolve(__dirname,'..','./src/index.tsx'),
-    resolve:{
-        extensions:['.tsx','.ts','.js'],
+
+module.exports = {
+    entry: path.resolve(__dirname, '..', './src/index.tsx'),
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.css'],
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.(ts|js)x?$/,
-                exclude:/node_modules/,
-                use:[
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
                     {
-                        loader:'babel-loader',
+                        loader: 'babel-loader',
                     },
                 ],
             },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'], // Add 'style-loader' before 'css-loader'
+            },
         ],
     },
-    output:{
-        path:path.resolve(__dirname,'..','./build'),
-        filename:'bundle.js',
+    output: {
+        path: path.resolve(__dirname, '..', './build'),
+        filename: 'bundle.js',
     },
-    plugins:[
+    plugins: [
         new HtmlWebpackPlugin({
-            template:path.resolve(__dirname,'..','./public/index.html'),
+            template: path.resolve(__dirname, '..', './public/index.html'),
         }),
     ],
-}
+};
