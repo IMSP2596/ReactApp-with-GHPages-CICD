1904b5ebd153b00fe9ac7a13ed69471314e88e35
Shubham Patil <Shubham.Patil@ingrammicro.com>
2024-12-23 19:58:50 +0530

    Fixing sonar cloud issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+// Get the root DOM element
+const rootElement = document.getElementById('root')!;
+
+// Create a root and render the app using createRoot
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
